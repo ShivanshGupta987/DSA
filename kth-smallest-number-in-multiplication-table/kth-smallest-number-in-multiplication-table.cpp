2aff5d2f102432cbee7feb5b6ae2f9e3553c0a6c
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2021-11-27 19:54:33 +0530

    Time: 16 ms (41.75%), Space: 5.9 MB (24.04%) - LeetHub

--- kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.cpp
+++ kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.cpp
@@ -1,31 +1,35 @@
 // class Solution {
 // public:
 //     int findKthNumber(int m, int n, int k) {
-//             vector<int>arr;
+//          int count=0;   
 //         for(int i=1;i<=m;i++){
 //                 for(int j=1;j<=n;j++){
-//                         arr.push_back(i*j);
+//                         if(k<=i*j)count++;
 //                 }
 //         }
-//             sort(arr.begin(),arr.end());
-//             return arr[k-1];
+//             return count;
 //     }
 // };
+
+
+
 class Solution {
 public:
+        // TC : O(mlog(m*n))
+        // SC: O(1)
     int findKthNumber(int m, int n, int k) {                                        
             int low=1; int high=m*n;
             while(low<high)
             {
-                 int mid=(low + high)/2;
-                    int count=0;
+                int mid=(low + high)/2;
+                int count=0;  // count stores how many numbers are smaller than or equal to mid
                     for(int i=1;i<=m;i++)
                     {
                         int j= mid/i;
                        count+= ( (j>n)? n : j);     
                     }
                     
-                    if(count>=k) high = mid;
+                    if(count>=k) high = mid; // target count is less than or equal to mid count
                     else low = mid +1 ;
             }
        return low;
