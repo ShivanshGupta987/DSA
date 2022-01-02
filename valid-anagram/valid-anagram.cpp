8a38878b4d43883f49182d47b47969a313252f3d
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-01-03 03:12:12 +0530

    Time: 18 ms (11.11%), Space: 7.3 MB (43.23%) - LeetHub

---
+++ valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+            unordered_map<char,int>mp;
+            
+            for(auto c:s){
+                    mp[c]++;
+            }
+            for(auto c:t){
+                    mp[c]--;
+                    if(mp[c]==0)mp.erase(c);
+            }
+            return mp.size()==0;
+        
+    }
+};
