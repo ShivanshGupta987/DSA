eb24cf97305a408de373946c7ab93a1ab7f0af64
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-01-10 21:43:21 +0530

    Time: 3 ms (13.05%), Space: 6.1 MB (11.68%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,23 +1,20 @@
 class Solution {
+        // DYNAMIC TABULATION APPROACH
         // TC: O(N^2)
-        // SC:O(N)
-        private : 
-        int dp(int n,vector<int>&mem){
-                if(n<=1)return mem[n]=1;
-                if(mem[n]>-1)return mem[n];
-                
-                int ans=0;
-                for(int i=1;i<=n;i++){
-                        ans+=dp(i-1,mem)*dp(n-i,mem);
-                }
-                return mem[n]=ans;
-        }
+        // SC: O(N)
 public:
     int numTrees(int n) {
-            
-        vector<int>mem(n+1,-1);
+        vector<int>mem(n+1);
             mem[0]=mem[1]=1;
-           return dp(n,mem);
+            
+            
+            for(int i=2;i<=n;i++){
+                    for(int j=1;j<=i;j++){
+                            mem[i]+=mem[j-1]*mem[i-j];
+                    }
+                    
+            }
+            return mem[n];
             
     }
 };
