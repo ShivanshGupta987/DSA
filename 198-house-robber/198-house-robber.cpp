943462c6af7eda62c59ec8c7a9a13743e81d99c1
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-01-12 22:49:12 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (71.58%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,47 @@
+class Solution {
+        // One - Liner Solution
+public:
+    int dp[101]{[0 ... 100]=-1};
+    int rob(vector<int>& nums,int i=0) {
+        return i<size(nums)?dp[i]!=-1?dp[i]:dp[i]=max(rob(nums,i+1),rob(nums,i+2)+nums[i]):0;
+    }
+};
+
+// class Solution {
+//         // TC: O(N)
+//         // SC: O(N) ->space taken by implicit recursive stack
+//         // N-> max depth of recursion
+        
+//         private: 
+//         int dp(vector<int>&nums,vector<int>&mem,int i){
+//                 if(size(nums)<=i)return 0;
+//                 if(mem[i]!=-1)return mem[i];
+//                 return mem[i]=max(dp(nums,mem,i+1),dp(nums,mem,i+2)+nums[i]);
+//         }
+// public:
+//     int rob(vector<int>& nums) {
+//         if(!nums.size())return 0;
+//             vector<int>mem(nums.size(),-1);
+//             return dp(nums,mem,0);
+            
+//     }
+// };
+
+
+
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//             // TC:O(N)
+//      int ans=0;
+//             int x=0,y=0;
+//             for(int i=0;i<nums.size();i+=2){
+//                x+=nums[i] ;   
+//             }
+//             for(int j=1;j<nums.size();j+=2){
+//                     y+=nums[j];
+//             }
+//             return ans=max(x,y);
+//     }
+// };
