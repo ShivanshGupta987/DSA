ce12e944ed2537a101e43296dc7a96055cafa446
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-01-17 23:24:15 +0530

    Time: 678 ms (12.95%), Space: 35.7 MB (44.68%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,21 +1,31 @@
-// TC: O(k*m*n)
-// SC: O(m*n)
-// k-> length of strs 
-
 class Solution {
 public:
+    int dp[601][101][101];
+    pair<int,int> get_count(string &s)
+    {
+        int one = 0,zero = 0;
+        for(char &ch : s)
+        {
+            if(ch=='0')
+                zero++;
+            else
+                one++;
+        }
+        return {zero,one};
+    }
+    int dfs(int i,int a,int b,vector<string> &strings,int &m,int &n)
+    {
+        if(i==strings.size())return 0;
+        if(dp[i][a][b]!=-1)return dp[i][a][b];
+        auto [x,y] = get_count(strings[i]);
+        if(a+x<=m && b+y<=n)
+            dp[i][a][b] = max(dfs(i+1,a+x,b+y,strings,m,n)+1,dfs(i+1,a,b,strings,m,n));
+        else
+            dp[i][a][b] = dfs(i+1,a,b,strings,m,n);
+        return dp[i][a][b];
+    }
     int findMaxForm(vector<string>& strs, int m, int n) {
-	vector<vector<int>>dp(m+1,vector<int>(n+1));
-            
-            for(auto str:strs){
-                    int zeros=count(str.begin(),str.end(),'0'), ones=str.size()-zeros;
-                    
-                    for(int i=m;i>=zeros;i--)
-                            for(int j=n;j>=ones;j--)
-                                    dp[i][j]=max(dp[i][j],  // current string is not choosen 
-                                        dp[i-zeros][j-ones]+1);// current string is choosen
-                    
-            }
-            return dp[m][n];
-}
+        memset(dp,-1,sizeof(dp));
+        return dfs(0,0,0,strs,m,n);
+    }
 };
