deafdce37c6874320811580199a420e9f1a79e35
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-01-21 16:33:33 +0530

    Time: 19 ms (43.34%), Space: 5.8 MB (98.85%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -3,27 +3,40 @@ class Solution {
         // SC: O(N)
         
 public:
-    int mem[1001]={[0 ... 1000]=0};
+    int mem[1001];
     int minSteps(int n) {
             if(n==1)return 0;
+            
             if(mem[n]!=0)return mem[n];
         
             for(int i=0;i<1001;i++)mem[i]=i;
 
             mem[0]=mem[1]=0;
            for(int i=2;i<1001;i++){
-                   int minn=mem[i];
+                   
+                   if(isPrime(i)){
+                          continue;
+                   }
+                   
+                   
                    for(int j=2;j<=34;j++){
                            if(i%j==0){
-                                   minn=min(minn,mem[i/j]+ j);
+                                  mem[i]=mem[i/j]+ j;
                                    break;
                            }
                           
                    }
-                   mem[i]=minn;
+                
            }
             return mem[n];
     }
+        bool isPrime(int n){
+                int primes[11] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 };
+                for(int i=0;i<11;i++){
+                        if(primes[i]==n)return true;
+                }
+                return false;
+        }
         
         
 };
