383e43ee5171ca71e8e8bceda5ae3fdeac83230d
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-01-25 20:40:12 +0530

    Time: 738 ms (6.82%), Space: 64.3 MB (22.16%) - LeetHub

---
+++ 956-tallest-billboard/956-tallest-billboard.cpp
@@ -0,0 +1,121 @@
+// DP 
+class Solution {
+        
+// Basically here we are dealing with difference of left beam and right beam instead of length of left beam and right beam
+        
+//         Notation  :  dp[d]
+                
+//       here dp[d] represents  length of the maximum possible smaller rod that we can have when len(left beam - right beam)==d
+//                 let us say dp[d]=y
+                
+//                 Visual representation 
+                
+//                        y
+//  left beam           --------
+// right beam          |--------|-------
+//                         y        d
+                
+// Initially we are assuming we have no rod hence dp[0]=0
+//           Now we are going to add a rod of  length x
+                
+//                 Case 1:   Adding the rod into the bigger beam
+        
+//                       y
+//  left beam           --------
+// right beam          |--------|------------
+//                         y        d+x
+                    
+//            Hence dp[d+x]=max(dp[d+x],y)    
+                        
+//                 Case 2:   Adding the rod into the smaller beam  and d>=x
+        
+//                       y         x
+//  left beam           --------|----|
+// right beam          |--------|-------
+//                         y        d
+                    
+//            Hence dp[d-x]=max(dp[d-x],y+x) 
+                        
+                        
+//                  Case 3:   Adding the rod into the smaller beam  and d<=x
+        
+//                       y         x
+//  left beam           --------|-----------|
+// right beam          |--------|-------
+//                         y        d
+                    
+//            Hence dp[x-d]=max(dp[x-d],y+d)
+                
+public:
+    int tallestBillboard(vector<int>& rods) {
+        unordered_map<int,int>dp;
+            dp[0]=0;
+         for(auto x: rods){
+                 unordered_map<int,int>cur(dp);
+                 
+                 for(auto it: cur){
+                         int d=it.first;
+                         
+                         dp[d+x]=max(dp[d+x],cur[d]);
+                         dp[abs(d-x)]= max(dp[abs(d-x)],cur[d]+min(d,x));
+                 }
+         }   
+            return dp[0];
+    }
+};
+
+// class Solution {
+        
+//         // Brute Force + Purning
+//         private: int maxx=0;
+       
+//      void rec(vector<int>& rods, int bkt1,int bkt2,int remain, int i){
+             
+//              if(bkt1==bkt2)maxx=max(maxx,bkt1);
+             
+//              if(i==rods.size())return;
+             
+//             if(bkt1 + bkt2 + remain<=2*maxx || abs(bkt1-bkt2)>remain)return;
+//              if(bkt1==bkt2 && remain==0)return;
+          
+//                   rec(rods,bkt1+rods[i],bkt2,remain-rods[i],i+1);
+                          
+//              rec(rods,bkt1,bkt2+rods[i],remain-rods[i],i+1);
+             
+//              rec(rods,bkt1,bkt2,remain,i+1);
+             
+
+//      }   
+// public:
+//     int tallestBillboard(vector<int>& rods) {
+//             int sum=0;
+//             for(auto x: rods)sum+=x;
+//             rec(rods,0,0,sum,0);
+//         return maxx;
+//     }
+// };
+
+
+// Brute Force
+// class Solution {
+       
+//      int  rec(vector<int>& rods, int bkt1,int bkt2, int i){
+             
+//              if(i==rods.size())return bkt1==bkt2?bkt1:0;
+             
+//           return max({
+//                   rec(rods,bkt1+rods[i],bkt2,i+1),
+                          
+//              rec(rods,bkt1,bkt2+rods[i],i+1),
+             
+//              rec(rods,bkt1,bkt2,i+1)
+//                       })   ;
+             
+
+//      }   
+// public:
+//     int tallestBillboard(vector<int>& rods) {
+//            return rec(rods,0,0,0);
+        
+//     }
+// };
