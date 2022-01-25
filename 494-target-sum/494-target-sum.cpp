5f06b9134e212e7affea790335617476ba41bda8
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-01-25 14:45:56 +0530

    Time: 33 ms (54.76%), Space: 12.4 MB (39.84%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -41,15 +41,15 @@ public:
             for(int i=0;i<n+1;i++)dp[i][0]=1;
             for(int i=1;i<n+1;i++){
                     for(int j=0;j<=new_sum;j++){
-                         if(j==0 && nums[i-1]==0)dp[i][j]=2*dp[i-1][j];
+  if(j==0 && nums[i-1]==0)dp[i][j]=2*dp[i-1][j];
  else if(nums[i-1]<=j) dp[i][j]+= (dp[i-1][j] + dp[i-1][j-nums[i-1]]);
          else dp[i][j]=dp[i-1][j];
                     }
             }
-            for(auto x:dp){
-                    for(auto y:x)cout<<y<<" ";
-                    cout<<endl;
-            }
+            // for(auto x:dp){
+            //         for(auto y:x)cout<<y<<" ";
+            //         cout<<endl;
+            // }
             return dp[n][new_sum];
             
     }
