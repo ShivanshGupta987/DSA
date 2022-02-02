e12f9fd0ea9c63668f60edd8f20d44c9a4c8296c
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-02 20:23:17 +0530

    Time: 191 ms (54.55%), Space: 9 MB (79.02%) - LeetHub

--- 879-profitable-schemes/879-profitable-schemes.cpp
+++ 879-profitable-schemes/879-profitable-schemes.cpp
@@ -8,9 +8,9 @@ public:
             vector<vector<int>>dp(G+1,vector<int>(P+1,0));
             for(int g=0;g<G+1;g++)dp[g][0]=1;
             for(int i=0;i<c;i++){
-               for(int g=G;g>=0;g--){
+               for(int g=G;g>=grp[i];g--){
                        for(int p=P;p>=0;p--){
-   if(g>=grp[i])  dp[g][p]= (dp[g][p] + dp[max(g-grp[i],0)][max(p-pr[i],0)]) %mod;
+    dp[g][p]= (dp[g][p] + dp[g-grp[i]][max(p-pr[i],0)]) %mod;
                                
                             //   cout<<dp[g][p]<<" ";
                        }
