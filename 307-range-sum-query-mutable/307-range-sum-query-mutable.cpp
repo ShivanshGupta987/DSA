206904a488e57dd1732a56ca910d331f25882e0a
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-03 22:29:57 +0530

    Time: 553 ms (51.43%), Space: 150.7 MB (81.66%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,60 +1,51 @@
 #define vi vector<int>
 class NumArray {
-public:
-     vector<int>st;
-     vector<int>arr;
-     int n;
-    public:
-      void ST(vi & arr){
-          n=arr.size();
-          this->arr=arr;
-          
-          // Height of ST
-          int x=(int)ceil(log2(n));
-          int max_size= 2*pow(2,x)-1;
-          st.resize(max_size);
-          constructST(0,n-1,0);
-     }
+ public:
+ vi bit;
+ vi arr;
+ int n;
+ public:
+   void BIT(vi &nums){
+         this->arr=nums;
+          n=nums.size();
+         bit.resize(n+1);
+        for(int i=0;i<n;i++){
+            init(i,nums[i]);
+        } 
+   }
+   int prefixSum(int ind){
+       if(ind<0)return 0;
+       ind++;
+       int sum=0;
+      while(ind>0){
+        sum+=bit[ind];
+        ind = ind - (ind & -ind);
+      }
+      return sum;
+   }
+   void init(int i, int val){
+        i++;
+       while(i<=n){
+           bit[i]+=val;
+           i= i+ (i&-i);
+       }
+   
+   }
+   void update(int i, int val){
+       int diff = val- arr[i];
+       arr[i]=val;
+      init(i,diff);
+   }
 
-     int constructST( int ss , int se , int si){
-      if(ss==se)  return st[si]=arr[ss];
-      int mid= ss+ (se-ss)/2;
-      return st[si]= constructST(ss,mid,si*2+1) + constructST(mid+1,se,si*2+2);
-     }
+   int sumRange(int l, int r){
+       return  prefixSum(r)-prefixSum(l-1);
+   }   
 
-     int getSum(int qs,int qe,int ss,int se,int si){
-         if(qs>se || ss>qe )return 0;
-
-         if(qs<=ss && qe>=se)return st[si];
-         int mid = ss + (se-ss)/2;
-
-         return getSum(qs,qe,ss,mid,2*si+1) + getSum(qs,qe,mid+1,se,2*si+2);
-     }
-    
-     
-     void update(int ss, int se, int i ,int si, int diff){
-         if(i<ss || i>se) return;
-         st[si]+= diff;
-         if(ss<se){
-             int mid= ss+ (se-ss)/2;
-             update(ss,mid,i,2*si+1,diff);
-             update(mid+1,se,i,2*si+2,diff);
-         }
-     }
-     void update(int i,int val){
-            int diff=val-arr[i];
-             arr[i]=val;
-         update(0,n-1,i,0,diff);
-     }
-    
     NumArray(vector<int>& nums) {
-        ST(nums);
+        BIT(nums);
     }
     
-    int sumRange(int qs, int qe) {
-        return getSum(qs,qe,0,n-1,0);
-    }
-        
+    
 };
 
 /**
