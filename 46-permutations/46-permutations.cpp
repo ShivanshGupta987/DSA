8b22b92d44cfdd6c616f3dd0da760574cece71ad
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-06 12:56:27 +0530

    Time: 10 ms (15.86%), Space: 7.9 MB (47.90%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,21 +1,25 @@
-#define vvi vector<vector<int>>
-#define vi vector<int>
 class Solution {
-        vvi res;
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        bt(0,nums);
-            return res;
-            
-    }
-        private:
-        void bt(int ind, vector<int>& nums){
-                if(ind==nums.size())res.push_back(nums);
-           for(int i=ind;i<nums.size();i++ ){
-                  swap(nums[i],nums[ind]);
-                   bt(ind+1,nums);
-                   swap(nums[i],nums[ind]);
-           }  
+    void helper(vector<int>&nums, vector<int>& track, vector<vector<int>>& ret) {
+        if(track.size() == nums.size()) {
+            vector<int> tmp(track);
+            ret.push_back(tmp);
+            return;
         }
         
+        for(int i = 0; i < nums.size(); ++i) {
+            auto it = find(track.begin(), track.end(), nums[i]);
+            if(it != track.end())
+                continue;
+            track.push_back(nums[i]);
+            helper(nums, track, ret);
+            track.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ret;
+        vector<int> track;
+        helper(nums, track, ret);
+        return ret;
+    }
 };
