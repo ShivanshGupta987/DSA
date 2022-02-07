46f4db4ba3a438740c2e6c754a5b6ad63d6e6d1d
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-07 12:09:30 +0530

    Time: 39 ms (78.67%), Space: 31.2 MB (77.92%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,11 +1,42 @@
 class Solution {
 public:
-        // TC: O(N)   // every element is visited atmost once
-        // SC: O(N)   // in worst case e.g. - [1,1,1,1,1,0]
-    bool canReach(vector<int>& arr, int i) {
-        return i>=0 && i<arr.size() && arr[i]>=0 && (!(arr[i]=-arr[i]) || canReach(arr,i-arr[i]) || canReach(arr,i+arr[i]));
+        // simulating bfs using queue
+        Solution(){
+                ios::sync_with_stdio(false);
+                cin.tie(0);
+                cout.tie(0);
+        }
+    bool canReach(vector<int>& arr, int start) {
+            int n=arr.size();
+        queue<int>q;
+            q.push(start);
+            while(!q.empty()){
+                 int i=q.front();
+                 q.pop();
+                    
+                 if(arr[i]==0)return true;
+                  
+                 int ni=i-arr[i]; // new_index      
+                 if(ni>=0 && ni<n && arr[ni]!=-1)q.push(ni);
+                 
+                 ni =i+arr[i]; 
+                 if(ni>=0 && ni<n && arr[ni]!=-1)q.push(ni);
+                    
+                arr[i]=-1;
+            }
+            return false;
     }
 };
+
+
+// class Solution {
+// public:
+//         // TC: O(N)   // every element is visited atmost once
+//         // SC: O(N)   // in worst case e.g. - [1,1,1,1,1,0]
+//     bool canReach(vector<int>& arr, int i) {
+//         return i>=0 && i<arr.size() && arr[i]>=0 && (!(arr[i]=-arr[i]) || canReach(arr,i-arr[i]) || canReach(arr,i+arr[i]));
+//     }
+// };
 
 
 // TC: O(N)
