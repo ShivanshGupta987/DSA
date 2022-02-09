f3ff2d599c241aecddf4cdd4e4ec22e13afc30d5
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-09 12:48:58 +0530

    Time: 293 ms (40.52%), Space: 66.9 MB (43.39%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
+        // TC:O(N)
+        // SC:O(p)
     int minSubarray(vector<int>& nums, int p) {
-            //TC:O(N)
-            // SC: O(p)
-        
-          int need=0;
-          
-          for(auto n:nums)need=(need+n)%p;
-          if(need==0)return 0; 
-
-          unordered_map<int,int>mp{{0,-1}};
-            int cur=0,len=INT_MAX;
-            for(int i=0;i<nums.size();i++){
-                   cur=(cur+nums[i])%p;
-                    mp[cur]=i;  
-                   int req=(cur-need+p)%p;
-                   if(mp.count(req)){
-                           len=min(len,i-mp[req]);
-                   } 
-                   
-                   
+        int len=INT_MAX;
+            int n=nums.size();
+            int need=0;
+            for(auto n:nums)need = (need+n)%p;
+            if(need==0)return 0;
+           
+            unordered_map<int,int>mp{{0,-1}};
+            int sum=0;
+            for(int i=0;i<n;i++){
+                    sum = (sum+nums[i])%p;
+                    mp[sum]=i;
+                    int req= (sum-need+p)%p;
+                  
+                    if(mp.count(req)){
+                          len=min(len,i-mp[req]);
+                    }
+                    
             }
-            
-            return (len==nums.size())?-1:len;
-   
-            
-            
-            
+            return len==nums.size()?-1:len;
     }
 };
