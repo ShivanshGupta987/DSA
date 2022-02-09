5e7d95b8bf29ee9308fe85bc4d7c08e894a01c13
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-09 14:38:29 +0530

    Time: 226 ms (41.43%), Space: 108.1 MB (37.44%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ #define mod 1000000007
+        // TC: O(N)
+        // SC: O(1)
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int count =0;
+          unordered_map<int,int>mp{{0,1},{1,0}};
+            int sum=0;
+            for(auto x :arr){
+                    sum=(sum+x)%2;
+                   if(sum%2){
+                           mp[1]++;
+                           count =(count + mp[0])%mod;
+                   }
+                    else {
+                            mp[0]++;
+                            count=(count + mp[1])%mod;
+                    }
+            }
+            return count;
+    }
+};
