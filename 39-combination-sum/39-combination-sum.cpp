2d3c4762994af9915ab85d304de5d35c82d8f3fd
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-11 01:44:51 +0530

    Time: 3 ms (96.29%), Space: 11 MB (52.81%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
-public:
-        // N->nums.size()
-        // min_nums->min(nums)
-        // TC : O(N^(min_nums +1) * (Target/min_nums))   
-        // SC :O(Target/min_nums) 
- void rec(int target,int ind,vector<int>&nums,vector<vector<int>>&res,vector<int>&vec){
-                if(target==0){res.push_back(vec);return;}
-               
-                for(int i=ind;i<nums.size();i++){
-                        if(nums[i]>target)break;
-                        vec.push_back(nums[i]); 
-                        rec(target-nums[i],i,nums,res,vec);
-                        vec.pop_back();               
-                }
-                
-        }
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        vector<vector<int>>res;
-            vector<int>vec;
-            sort(nums.begin(),nums.end());
-         rec(target,0,nums,res,vec);
-             return res;
-    }
-       
+    vector<vector<int>>res;
+        void bt(vector<int>& a, int target,int ind,vector<int>& temp){
         
+                if(target==0){
+                        res.push_back(temp);
+                        return;
+                }
+                for(int i=ind;i<a.size();i++){
+                        
+                        if(target-a[i]<0)break;
+                        temp.push_back(a[i]);
+                        bt(a,target-a[i],i,temp);  
+                        temp.pop_back();
+                }
+        }
+public:
+    vector<vector<int>> combinationSum(vector<int>& a, int target) {
+           sort(a.begin(),a.end());
+            vector<int>temp;
+            bt(a,target,0,temp);
+            
+            return res;
+    }
 };
