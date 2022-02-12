e2bc0f71386a8b496404a044f7ac0f2e18f11d3f
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-12 13:14:59 +0530

    Time: 13 ms (52.52%), Space: 11 MB (58.81%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,25 +1,21 @@
 class Solution {
-    vector<vector<int>>res;
-        void bt(vector<int>& a, int target,int ind,vector<int>& temp){
-        
+        vector<vector<int>>res;
+        void bt(int ind,vector<int>&arr, int target,vector<int>&temp){
                 if(target==0){
-                        res.push_back(temp);
-                        return;
+                        res.push_back(temp);return;
                 }
-                for(int i=ind;i<a.size();i++){
-                        
-                        if(target-a[i]<0)break;
-                        temp.push_back(a[i]);
-                        bt(a,target-a[i],i,temp);  
+                for(int i=ind; i<arr.size();i++){
+                        if(target-arr[i]<0)break;
+                        temp.push_back(arr[i]);
+                        bt(i,arr,target-arr[i],temp);
                         temp.pop_back();
                 }
         }
 public:
-    vector<vector<int>> combinationSum(vector<int>& a, int target) {
-           sort(a.begin(),a.end());
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
             vector<int>temp;
-            bt(a,target,0,temp);
-            
+            bt(0,arr,target,temp);
             return res;
     }
 };
