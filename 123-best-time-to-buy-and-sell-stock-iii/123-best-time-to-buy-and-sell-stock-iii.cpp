485a3c898f973f6a5691922f11a1954cbb89eab1
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-23 22:22:11 +0530

    Time: 235 ms (41.07%), Space: 87.7 MB (20.82%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,19 @@
 class Solution {
-        // TC: O(K*N)
-        // SC: O(K*N)
-        // K-> MAX NO. OF TRANSACTIONS
-        // N-> length of prices array
+        // TC:O(N*K)
+        // SC:O(N*K)  
+        // K-> total no. of tranasactions
 public:
     int maxProfit(vector<int>& prices) {
+        const int k=2;
         int n=prices.size();
-            int k=2;
-            
-            vector<vector<int>>dp(k+1,vector<int>(n,0));
-            
-            for(int i=1;i<=k;i++){
-                    
-                    int mindiff=prices[0];
-                    for(int j=1;j<n;j++){
-                            mindiff=min(mindiff,prices[j]-dp[i-1][j-1]);
-                            dp[i][j]=max(dp[i][j-1],prices[j]-mindiff);
-                            //cout<<dp[i][j]<<" ";
-                           
-                    }
-                    cout<<endl;
-            }
-            return dp[2][n-1];
+        vector<vector<int>>dp(k+1,vector<int>(n,0));
+       for(int i=1;i<=k;i++){
+               int max_diff=-prices[0];
+               for(int j=1;j<n;j++){   
+                    dp[i][j]=max(dp[i][j-1],prices[j]+max_diff);
+                    max_diff=max(max_diff,dp[i-1][j]-prices[j]);
+               }
+       }
+      return dp[k][n-1];      
     }
 };
