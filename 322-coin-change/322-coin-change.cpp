f0a761dc5dd9bb07e40adc4f7b43f709f7d2cf58
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-25 12:56:00 +0530

    Time: 64 ms (88.41%), Space: 14.4 MB (65.77%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,29 +1,62 @@
 class Solution {
+public:  
         // TC : O(S*N)
         // SC : O(S)
         // S -> AMOUNT
         // N -> NO. OF COINS
-private:
-    int helper(vector<int>& coins,vector<int>& amt,int target){
-                if(target==0)return 0;
-                if(target<0)return -1;
-                
-                if(amt[target]!=0)return amt[target];
-                 
-                int min_cost = INT_MAX;
-                for(auto coin : coins){
-                   int res = helper(coins, amt, target-coin);
-                   if(res!=-1)min_cost=min(min_cost,res+1);
-                }
-            return amt[target]= min_cost== INT_MAX ? -1 : min_cost;
-        }
-public:
+        // BOTTOM-UP DP 
+        
+// ------------ INTUTION BEHIND THE PROBLEM--------------
+//         -> LET US SAY OUR function for the problem is :   F(S)
+//            where S is the amount to be gained  
+//                  F(S) is the mininum no. of coins required to gain S
+//         -> Hence , Recurrence relation is : 
+//                   F(S) = min( F(S-Ci) where Ci:[0,N) ) + 1
+//                   and Ci <= S      
+//         -> base case F(S)=0                                        
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>amt(amount+1);
-            return helper(coins, amt, amount);
-            
+        int maxx = (int) 1e5;
+        vector<int>dp(amount+1,maxx);
+            dp[0]=0;
+            int n=coins.size();    
+            for(int s=1; s<= amount; s++){
+                    for(auto coin : coins){
+                         if(coin<=s) dp[s]= min(dp[s],dp[s-coin]+1);
+                    }
+            }
+            return dp[amount]==maxx ? -1 : dp[amount];
     }
 };
+
+
+
+// class Solution {
+//         // TC : O(S*N)
+//         // SC : O(S)
+//         // S -> AMOUNT
+//         // N -> NO. OF COINS
+//         // top-down dp
+// private:
+//     int helper(vector<int>& coins,vector<int>& amt,int target){
+//                 if(target==0)return 0;
+//                 if(target<0)return -1;
+                
+//                 if(amt[target]!=0)return amt[target];
+                 
+//                 int min_cost = INT_MAX;
+//                 for(auto coin : coins){
+//                    int res = helper(coins, amt, target-coin);
+//                    if(res!=-1)min_cost=min(min_cost,res+1);
+//                 }
+//             return amt[target]= min_cost== INT_MAX ? -1 : min_cost;
+//         }
+// public:
+//     int coinChange(vector<int>& coins, int amount) {
+//         vector<int>amt(amount+1);
+//             return helper(coins, amt, amount);
+            
+//     }
+// };
 
 
 
