7ddd4c439a63448f0187cf88e95ec62001ff306c
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-25 16:34:10 +0530

    Time: 3 ms (95.50%), Space: 7.9 MB (75.17%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,49 @@
+class Solution {
+        // TC : O(N)
+        
+// ---------------- INTUTION BEHIND THE PROBLEM ------------------
+        
+//    Problem : To find out no. of setbits in given number n
+         
+//   # Case 1 # (if n is even)
+//            no. of setbits in n = no. of setbits in n/2
+//       Ex - 
+//            10 = 1010
+//            5  = 0101
+//   # Case 2 # (if n is odd)
+//            no. of setbits in n = (no. of setbits in n/2) + 1;   
+//       Ex- 
+//            11 = 1011
+//             5 = 0101
+public:
+    vector<int> countBits(int n) {
+        
+            vector<int> dp(n+1);
+            
+            for(int i=0;i<=n;i++){
+                  dp[i] = dp[i/2] + i%2;  
+            }
+            
+            return dp;
+    }
+};
+
+
+//class Solution {
+//         // TC : O(N log N)
+//         // SC : O(1)
+//         void countSetBits(vector<int>& ans, int n){
+//                 int x=n;
+//                 while(x!=0)
+//                 {
+//                  ans[n]++; 
+//                  x &= x-1; // diminshes the first setbit in x 
+//                 }
+//         }
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> ans(n+1);
+//             for(int i=0;i<=n;i++)countSetBits(ans,i);
+//             return ans;
+//     }
+// };
