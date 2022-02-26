43487b6c4c21426d160394f42bf795ddb15527fa
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-02-26 13:20:19 +0530

    Time: 146 ms (36.63%), Space: 14.5 MB (29.33%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,95 +1,15 @@
 class Solution {
-public:  
-        // TC : O(S*N)
-        // SC : O(S)
-        // S -> AMOUNT
-        // N -> NO. OF COINS
-        // BOTTOM-UP DP 
-        
-// ------------ INTUTION BEHIND THE PROBLEM--------------
-//         -> LET US SAY OUR function for the problem is :   F(S)
-//            where S is the amount to be gained  
-//                  F(S) is the mininum no. of coins required to gain S
-//         -> Hence , Recurrence relation is : 
-//                   F(S) = min( F(S-Ci) where Ci:[0,N) ) + 1
-//                   and Ci <= S      
-//         -> base case F(S)=0                                        
+        // Recurrence Relation -> optimal(F(S)) = optimal (F(S-Ci)) + 1 where i : [0,N)
+public:
     int coinChange(vector<int>& coins, int amount) {
-        int maxx = (int) 1e5;
-        vector<int>dp(amount+1,maxx);
+            int max = 100000;
+            vector<int>dp(amount+1,max);
             dp[0]=0;
-            int n=coins.size();    
-            for(int s=1; s<= amount; s++){
-                    for(auto coin : coins){
-                         if(coin<=s) dp[s]= min(dp[s],dp[s-coin]+1);
+            for(int s=1; s<= amount ; s++){
+                    for(int coin : coins){
+                            if(s>=coin) dp[s] = min(dp[s],dp[s-coin]+1);
                     }
             }
-            return dp[amount]==maxx ? -1 : dp[amount];
+            return dp[amount]==max ? -1 : dp[amount];
     }
 };
-
-
-
-// class Solution {
-//         // TC : O(S*N)
-//         // SC : O(S)
-//         // S -> AMOUNT
-//         // N -> NO. OF COINS
-//         // top-down dp
-// private:
-//     int helper(vector<int>& coins,vector<int>& amt,int target){
-//                 if(target==0)return 0;
-//                 if(target<0)return -1;
-                
-//                 if(amt[target]!=0)return amt[target];
-                 
-//                 int min_cost = INT_MAX;
-//                 for(auto coin : coins){
-//                    int res = helper(coins, amt, target-coin);
-//                    if(res!=-1)min_cost=min(min_cost,res+1);
-//                 }
-//             return amt[target]= min_cost== INT_MAX ? -1 : min_cost;
-//         }
-// public:
-//     int coinChange(vector<int>& coins, int amount) {
-//         vector<int>amt(amount+1);
-//             return helper(coins, amt, amount);
-            
-//     }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class Solution {
-//         // BRUTE FORCE
-//         // TLE
-//         // EXPONENTIAL TC
-//     private :  
-//         int helper(vector<int>& coins, int target, int i){
-//                 if(target==0)return 0;
-//                 if(target<0 || coins.size()==i)return -1;
-                
-//                 int max_coins = target/coins[i];
-//                 int min_cost=INT_MAX; // minimum required coins
-//                 for(int x=0; x<= max_coins ; x++){
-//                         int ans = helper(coins, target-x*coins[i], i+1);
-//                         if(ans!=-1) min_cost=min(min_cost,x+ans);
-//                 }
-//                 return min_cost==INT_MAX? -1 : min_cost;
-//         }
-// public:
-//     int coinChange(vector<int>& coins, int amount) {
-//      return  helper(coins,amount,0);   
-//     }
-// };
