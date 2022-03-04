f2142f460601b5b574eed805738bc60602d837d0
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-03-04 11:10:38 +0530

    Time: 22 ms (57.10%), Space: 8.6 MB (29.94%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,14 +1,38 @@
 class Solution {
-int dp[501]={};
-        // TC : O(n*K)
-        // SC : O(n)
+        // TC : O(N*K)
+        // SC : O(N)
+        
 public:
-    int maxSumAfterPartitioning(vector<int>& arr, int k,int pos=0, int res=0) {
-        if(pos<arr.size() && dp[pos]!=0) return dp[pos];
-            for(int i=1, mv = 0; i<=k && i+pos<= arr.size(); i++){
-                    mv = max(mv, arr[pos+i-1]);
-                    res = max(res, mv*i + maxSumAfterPartitioning(arr,k,pos+i));
+    int maxSumAfterPartitioning(vector<int>& arr, int K) {
+            int n = arr.size();
+            vector<int>dp(n+1);
+            
+            for(int i=1; i<=n; i++){
+                    int cur_max=0, best =0;
+                    for(int k=1;k<=K && i-k>=0 ;k++){
+                            cur_max = max(cur_max , arr[i-k]);
+                            best = max(best, dp[i-k]+ cur_max*k);
+                    }
+                    dp[i]=best;
             }
-            return dp[pos]=res;
+            return dp[n];
     }
 };
+
+
+
+
+// class Solution {
+// int dp[501]={};
+//         // TC : O(n*K)
+//         // SC : O(n)
+// public:
+//     int maxSumAfterPartitioning(vector<int>& arr, int k,int pos=0, int res=0) {
+//         if(pos<arr.size() && dp[pos]!=0) return dp[pos];
+//             for(int i=1, mv = 0; i<=k && i+pos<= arr.size(); i++){
+//                     mv = max(mv, arr[pos+i-1]);
+//                     res = max(res, mv*i + maxSumAfterPartitioning(arr,k,pos+i));
+//             }
+//             return dp[pos]=res;
+//     }
+// };
