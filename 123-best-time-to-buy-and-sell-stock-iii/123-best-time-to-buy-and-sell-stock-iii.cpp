7df16b4b7724a544fbd73f360400c46d1cb7f24a
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-03-09 22:43:00 +0530

    Time: 185 ms (64.26%), Space: 87.7 MB (20.88%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,52 +1,30 @@
 class Solution {
+        // TC : O(N*K)
+        // SC : O(N*K)
+        // N -> size(prices)
+        // K -> No. of transactions
 public:
-    
     int maxProfit(vector<int>& prices) {
-        
-        int dp[prices.size()+1 ][5];
-        reverse(prices.begin(),prices.end());
-        
-        for(int day = 0; day<=prices.size(); day++){
-            
-            for(int transactionsDone = 0;transactionsDone <= 4;transactionsDone++){
-                
-                int &ans = dp[day][transactionsDone];
-                
-                if(day ==0){
-                    ans = 0;
-                }else if(transactionsDone == 0){
-                    ans = 0;
-                }else{
-                    
-                    // choice 1
-                    // no transaction today
-                    int ans1 = dp[day-1][transactionsDone];
-                    
-                    // choice 2
-                    // doing the possible transaction today     
-                    int ans2 = 0;
-                    bool buy = (transactionsDone % 2 == 0);
-
-                    if(buy == true){ // buy
-                        ans2 = -prices[day-1] + dp[day - 1][transactionsDone - 1];
-                    }else{ // sell
-                        ans2 = prices[day-1] + dp[day - 1][transactionsDone - 1];
+            int n = prices.size();
+            vector<vector<int>>dp(3,vector<int>(n));
+            for(int k=1;k<=2;k++)
+            {
+                    int maxx_diff = -prices[0];
+                    for(int i=1;i<n;i++)
+                    {
+                         int max_profit = dp[k][i-1];
+                        
+                         // for(int j=0;j<i;j++)
+                         // {
+                         // max_profit= max(max_profit, prices[i]-prices[j]+ dp[k-1][j]);
+                         // }
+                         max_profit = max(max_profit, prices[i] + maxx_diff);
+                         dp[k][i]= max_profit;
+                         maxx_diff = max(maxx_diff , -prices[i] + dp[k-1][i]);
                     }
-
-                    ans = max(ans1, ans2);
-
-                }
-                
+                    
             }
-        }
             
-//         for(int day = (int) prices.size();day >= 0;day--){
-//             for(int transactionsLeft = 0;transactionsLeft <= 4;transactionsLeft++){
-//                 cout<<dp[day][transactionsLeft]<<" ";
-//             }
-//                 cout<<endl;
-//         }
-            
-        return dp[prices.size()][4];
+            return dp[2][n-1];
     }
 };
