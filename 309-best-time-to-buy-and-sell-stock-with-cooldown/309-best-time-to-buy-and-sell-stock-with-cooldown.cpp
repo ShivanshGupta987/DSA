17f6b44fb156324119de4d4f9e282d4e2f85c6e8
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-03-13 17:14:27 +0530

    Time: 4 ms (76.42%), Space: 11.3 MB (57.95%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,5 +1,5 @@
 class Solution {
-        // TC : O(N*N)
+        // TC : O(N)
         // SC : O(N)
 public:
     int maxProfit(vector<int>& prices) {
@@ -7,15 +7,27 @@ public:
             int n = prices.size();
             vector<int> cool(n), dp(n);
             
-         // dp[i] -> max profit obtained till ith day 
+         // dp[i] -> max profit obtained till ith day by selling it either ith day or not selling it
+        // cool[i] -> max profit obtained till ith day by cooling down at ith day 
+            
+            int max_diff = -prices[0];
             
             for(int i=1;i<n;i++)
             {       
-                    dp[i] = max(dp[i],prices[i]-prices[0]);
-                    for(int j=1;j<i;j++){
-                    dp[i] = max(dp[i],cool[j-1] + prices[i]-prices[j]);
-                    cool[i] = max(dp[i-1],cool[i-1]);
-                    }
+                    
+                    
+                    // dp[i] = max(dp[i],prices[i]-prices[0]);
+                    // for(int j=1;j<i;j++){
+                    // dp[i] = max(dp[i],cool[j-1] + prices[i]-prices[j]);
+                    // cool[i] = max(dp[i-1],cool[i-1]);
+                    // }
+                    
+                    dp[i] = max(dp[i],prices[i] + max_diff);
+                    
+                    cool[i] =  max(dp[i-1] , cool[i-1]);
+                    
+                    max_diff = max(max_diff, cool[i-1] - prices[i]);
+                    
             }
            return max(cool[n-1],dp[n-1]);
              
