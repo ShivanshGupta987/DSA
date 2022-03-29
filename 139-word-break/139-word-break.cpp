a05d5fa11ad69148c90d8c794f8bcca4f7c58b86
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-03-29 17:06:31 +0530

    Time: 12 ms (61.46%), Space: 13.3 MB (39.99%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,63 @@
+class Solution {
+        // DP APPROACH
+        // TC : O(N*N)
+        
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+            unordered_set<string>ss;
+           
+            for(auto & x: wordDict) ss.insert(x);
+            
+            int n = s.size();
+            vector<bool>dp(n+1,false);
+            // dp[i] = true ; implies s[0,i) is present in wordDict
+            dp[0] = true;
+            for(int i=1;i<=n;i++){
+                    for(int j=0; j<i;j++){
+                            if(dp[j] && ss.count(s.substr(j,i-j))) {
+                                    dp[i]=true;
+                                    break;
+                            }
+                    }
+            }
+            return dp[n];
+    }
+};
+
+
+// class Solution {
+//         // BRUTE FORCE 
+//         // TC : O(2^N)
+//         // SC : O(N)
+//         // N -> s.size()
+        
+// public:
+//     bool wordBreak(string s, vector<string>& wordDict) {
+        
+//             unordered_set<string> ss;
+//             for(auto &x : wordDict)ss.insert(x);
+            
+//             return dfs(s,ss);
+            
+//     }
+        
+//         bool dfs(string s, unordered_set<string>& ss){
+//                 int n = s.size();
+//                 if(n==0) return true;
+          
+                
+//                 for(int end = 0; end<n; end++){
+                        
+//                         string str = s.substr(0,end+1);
+                        
+//                         if((ss.count(str)) && dfs(s.substr(end+1),ss)){
+//                                 return true;
+//                         }
+                        
+                        
+//                 }
+                
+//                 return false;
+//         }
+// };
