1ad755d4c24a729b0e674785f9e317ec995eba2d
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-03-30 19:10:02 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (27.77%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,26 +1,65 @@
 class Solution {
-        vector<string>ans;
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-       
-            unordered_set<string>ss;
-            for(auto x: wordDict) ss.insert(x);
-            dfs(0,s,ss,"");
-            return ans;
-    }
-        
-    void dfs(int start, string s, unordered_set<string>& ss, string cur_str){
+     
+            int n = s.size();
+            unordered_set<string>words;
+            for(auto &x : wordDict) words.insert(x);
             
-            int len = s.size();
-            if(len==0) {
-                    ans.push_back(cur_str); return ;
-            }
+            vector<vector<string>>dp(n+1,vector<string>());
+            dp[n].push_back("");
+            // dp[i] -> vector of strings starting from index i to end of the string
             
-            for(int end=0; end<len; end++){
-                    string str = s.substr(0,end+1);
-                    if(ss.count(str)) {
-              dfs(end+1, s.substr(end+1),ss, cur_str + (cur_str.size()?" ":"") + str);
+            for(int i=n-1; i>=0; i--){
+                    vector<string>values;
+                    for(int j=i+1; j<=n; j++){
+                            string str = s.substr(i,j-i);
+                            if(words.count(str)){
+                                    for(int k=0; k< dp[j].size();k++){
+                                            values.push_back(str + (dp[j][k].empty()?"":" ") + dp[j][k]);
+                                    }
+                            }
                     }
+                    dp[i]= values;
             }
+            return dp[0];
     }
 };
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//         // TC : O(2^N)
+//         // N -> s.size()
+//         vector<string>ans;
+// public:
+//     vector<string> wordBreak(string s, vector<string>& wordDict) {
+       
+//             unordered_set<string>ss;
+//             for(auto x: wordDict) ss.insert(x);
+//             dfs(0,s,ss,"");
+//             return ans;
+//     }
+        
+//     void dfs(int start, string s, unordered_set<string>& ss, string cur_str){
+            
+//             int len = s.size();
+//             if(len==0) {
+//                     ans.push_back(cur_str); return ;
+//             }
+            
+//             for(int end=0; end<len; end++){
+//                     string str = s.substr(0,end+1);
+//                     if(ss.count(str)) {
+//               dfs(end+1, s.substr(end+1),ss, cur_str + (cur_str.size()?" ":"") + str);
+//                     }
+//             }
+//     }
+// };
