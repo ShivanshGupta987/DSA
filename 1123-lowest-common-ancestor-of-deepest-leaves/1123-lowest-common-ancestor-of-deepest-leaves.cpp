10f66944f354913d9c28d8dfe7e8c1d67583f406
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-01 21:29:37 +0530

    Time: 12 ms (90.53%), Space: 21.2 MB (62.84%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -10,32 +10,67 @@
  * };
  */
 class Solution {
-        
-        // TC : O(N)
-        // SC : O(H)
-        // N -> NO. OF NODES
-        // H -> HEIGHT OF THE TREE
-        
-    TreeNode* lca;
-    int deepest = 0;
 public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        
-            fun(root, 0);
-            return lca;
+       return helper(root).first; 
+            
     }
-        
-    int fun(TreeNode* root , int depth){
+
+    pair<TreeNode* , int> helper(TreeNode* root){
             
-            deepest = max(deepest , depth);
+            if(root == NULL) return {NULL, 0};
             
-            if(root == NULL) return depth;
+            auto left = helper(root->left);
+            auto right = helper(root -> right);
             
-            int left = fun(root->left, depth+1);
-            int right = fun(root-> right , depth +1);
+            if(left.second > right.second) return {left.first , left.second + 1};
+            else if(left.second < right.second) return {right.first , right.second +1};
+            else return {root,left.second+1};
             
-            if(left == deepest && right == deepest ) lca = root;
             
-            return max(left , right);
     }
 };
+
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+        
+//         // TC : O(N)
+//         // SC : O(H)
+//         // N -> NO. OF NODES
+//         // H -> HEIGHT OF THE TREE
+        
+//     TreeNode* lca;
+//     int deepest = 0;
+// public:
+//     TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        
+//             fun(root, 0);
+//             return lca;
+//     }
+        
+//     int fun(TreeNode* root , int depth){
+            
+//             deepest = max(deepest , depth);
+            
+//             if(root == NULL) return depth;
+            
+//             int left = fun(root->left, depth+1);
+//             int right = fun(root-> right , depth +1);
+            
+//             if(left == deepest && right == deepest ) lca = root;
+            
+//             return max(left , right);
+//     }
+// };
