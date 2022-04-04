6c6e175e5aa63b8e1b07f580e63c92303abbda22
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-04 15:11:48 +0530

    Added solution - LeetHub

--- K-Palindrome - GFG/kpalindrome.cpp
+++ K-Palindrome - GFG/kpalindrome.cpp
@@ -13,23 +13,44 @@ public:
     {
         // code here
         
-        string s1 = str , s2 = string (str.rbegin(),str.rend());
+        // TC : O(N^2)
+        
+        // string s1 = str , s2 = string (str.rbegin(),str.rend());
        
-        vector<vector<int>>dp(n+1,vector<int>(n+1));
+        // vector<vector<int>>dp(n+1,vector<int>(n+1));
         
-        for(int i=1;i<=n;i++){
-            dp[i][0] = i; dp[0][i] = i; 
-        }
+        // for(int i=1;i<=n;i++){
+        //     dp[i][0] = i; dp[0][i] = i; 
+        // }
         
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = min(dp[i-1][j], dp[i][j-1])+1;
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n; j++){
+        //         if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
+        //         else dp[i][j] = min(dp[i-1][j], dp[i][j-1])+1;
+        //     }
+        // }
+        
+        // return (dp[n][n]/2 <= k);
+        
+        
+        int start = 0 , end = str.size()-1;
+        
+        if(str.size()==1) return 1;
+        
+        while(start < end){
+            if(str[start]!=str[end]){
+                if(k>0){
+                    k--;
+                    if(str[start]==str[end-1]) start--;
+                    else end++;
+                }
+                else return 0;
             }
+            start++;
+            end--;
         }
-        
-        return (dp[n][n]/2 <= k);
      
+     return 1;
         
     }
     
