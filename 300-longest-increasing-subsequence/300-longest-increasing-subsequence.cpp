5ea168327aa8242050181efe09346af7b9ff20db
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-05 20:52:18 +0530

    Time: 12 ms (83.84%), Space: 10.5 MB (65.05%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -10,20 +10,23 @@ public:
             for(int k=1;k<n;k++){
                   if(nums[k]>a.back())a.push_back(nums[k]); 
                     else {
-                            int ans=0;
-                            int i=0,j=a.size()-1;
-                            while(i<=j){
-                                    int mid= i+(j-i)/2;
+//                             int ans=0;
+//                             int i=0,j=a.size()-1;
+//                             while(i<=j){
+//                                     int mid= i+(j-i)/2;
                                     
-                                    if(a[mid]>=nums[k]){
-                                            ans=mid;
-                                            j=mid-1;
-                                    }
-                                    else {
-                                            i=mid+1;
-                                    }
-                            }
-                             a[ans]=nums[k];
+//                                     if(a[mid]>=nums[k]){
+//                                             ans=mid;
+//                                             j=mid-1;
+//                                     }
+//                                     else {
+//                                             i=mid+1;
+//                                     }
+//                             }
+                            
+               int ans = lower_bound(a.begin(),a.end(), nums[k]) - a.begin();
+               a[ans]=nums[k];
+                            
                             
                     }
             }
