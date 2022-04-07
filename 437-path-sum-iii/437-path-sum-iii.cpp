542e2c35882e3f0cac5ba05148002dd9feeb5e00
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-07 19:01:57 +0530

    Time: 1325 ms (5.03%), Space: 191.2 MB (5.56%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+        // TC : O(N)
+        // N -> NO. OF NODES
+        // SC : O(H)
+        // H -> HEIGHT OF THE TREE
+        int ans;
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        
+            ans = 0;
+            unordered_map<long long,int>mp;
+            mp[0]=1;
+            dfs(root, targetSum, mp,0);
+            return ans;
+    }
+        
+        void dfs(TreeNode* root, int target, unordered_map<long long,int> mp, long long pre){
+                
+                 if(root==NULL) return;
+                 pre += root->val;
+                 
+                if(mp.find(pre-target)!=mp.end())  
+                        ans += mp[pre-target];
+                mp[pre]++;
+                dfs(root -> left, target , mp , pre);
+                dfs(root -> right, target, mp, pre);
+        }
+};
