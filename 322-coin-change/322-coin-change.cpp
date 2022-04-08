5c463ea2b8c636f6d170deb5962cce6d7fb63ec5
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-08 19:09:25 +0530

    Time: 165 ms (26.51%), Space: 14.5 MB (53.33%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,15 +1,15 @@
 class Solution {
-        // Recurrence Relation -> optimal(F(S)) = optimal (F(S-Ci)) + 1 where i : [0,N)
 public:
     int coinChange(vector<int>& coins, int amount) {
-            int max = 100000;
-            vector<int>dp(amount+1,max);
+        
+            int MAX = 100000;
+            vector<int> dp(amount+1,MAX);
             dp[0]=0;
-            for(int s=1; s<= amount ; s++){
+            for(int i=1;i<=amount;i++){
                     for(int coin : coins){
-                            if(s>=coin) dp[s] = min(dp[s],dp[s-coin]+1);
+                        if(i>=coin) dp[i] = min(dp[i],dp[i-coin]+1); 
                     }
             }
-            return dp[amount]==max ? -1 : dp[amount];
+            return dp[amount]==MAX ? -1 : dp[amount];
     }
 };
