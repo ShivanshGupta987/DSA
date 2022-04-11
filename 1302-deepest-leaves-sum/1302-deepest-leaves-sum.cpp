b783b4e124dbdf07c924a2489bafd7d372596b2d
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-11 20:04:57 +0530

    Time: 106 ms (80.21%), Space: 61.8 MB (28.52%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -17,17 +17,60 @@
 class Solution {
 public:
     int deepestLeavesSum(TreeNode* root) {
-        if(root==NULL)return 0;
-            int h = height(root)-1;
-            return dfs(root,0,h);
+            if(root==NULL)return 0;
+            queue<TreeNode*> q;
+            q.push(root);
+            
+            int res = 0;
+            
+            while(!q.empty())
+            {
+                    res = 0;
+                   for(int i = q.size()-1; i>=0 ; i--)
+                   {
+                           TreeNode* temp = q.front();
+                           q.pop();
+                           res += temp->val;
+                           
+                           if(temp->left) q.push(temp->left);
+                           if(temp->right) q.push(temp->right);
+                   }
+            }
+            
+            return res;
+            
     }
-        int height(TreeNode* root){
-                if(root==nullptr) return 0;
-                return max(height(root->left)+1 , height(root->right)+1);
-        }
-        int dfs(TreeNode* root, int level, int h){
-                if(root==nullptr)return 0;
-                if(root->left==root->right && level==h) return root->val;
-                return dfs(root->left,level+1,h) + dfs(root->right,level+1,h);
-        }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// // TC : O(N)
+// // SC : O(H)
+// //  N -> NO. OF NODES IN THE TREE
+// // H -> HEIGHT OF THE TREE
+// class Solution {
+// public:
+//     int deepestLeavesSum(TreeNode* root) {
+//         if(root==NULL)return 0;
+//             int h = height(root)-1;
+//             return dfs(root,0,h);
+//     }
+//         int height(TreeNode* root){
+//                 if(root==nullptr) return 0;
+//                 return max(height(root->left)+1 , height(root->right)+1);
+//         }
+//         int dfs(TreeNode* root, int level, int h){
+//                 if(root==nullptr)return 0;
+//                 if(root->left==root->right && level==h) return root->val;
+//                 return dfs(root->left,level+1,h) + dfs(root->right,level+1,h);
+//         }
+// };
