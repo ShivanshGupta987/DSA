10f479afa9195c215bb00289ca8be79c295bca50
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-11 19:53:12 +0530

    Time: 206 ms (7.97%), Space: 59.8 MB (76.73%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// TC : O(N)
+// SC : O(H)
+//  N -> NO. OF NODES IN THE TREE
+// H -> HEIGHT OF THE TREE
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        if(root==NULL)return 0;
+            int h = height(root)-1;
+            return dfs(root,0,h);
+    }
+        int height(TreeNode* root){
+                if(root==nullptr) return 0;
+                return max(height(root->left)+1 , height(root->right)+1);
+        }
+        int dfs(TreeNode* root, int level, int h){
+                if(root==nullptr)return 0;
+                if(root->left==root->right && level==h) return root->val;
+                return dfs(root->left,level+1,h) + dfs(root->right,level+1,h);
+        }
+};
