0d6d7f4a12fe149fc06a48757804f43357855414
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-12 14:32:06 +0530

    Time: 239 ms (57.12%), Space: 104.9 MB (91.08%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -9,38 +9,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-        // TC : O(N)
-        // N -> NO. OF NODES IN THE TREE
-
+        vector<int>sums;
 public:
     int maxLevelSum(TreeNode* root) {
         
-            queue<TreeNode*> q;
-            q.push(root);
-            int max_sum = INT_MIN;
-            int level = 0;
-            int ans;
-            while(!q.empty()){
-                    
-                    level++;
-                    int sum = 0;
-                    
-                    for(int i = q.size()-1; i>=0; i--){
-                            
-                            TreeNode* temp = q.front();
-                            q.pop();
-                            
-                            sum += temp->val;
-                            if(temp->left) q.push(temp->left);
-                            if(temp->right)q.push(temp->right);
-                    }
-                    if(sum>max_sum) {
-                            max_sum = sum;
-                            ans = level;
-                    } 
-            }
-            return ans;
+            dfs(root,1);
+            return distance(sums.begin(),max_element(sums.begin(),sums.end())) + 1;
+           
     }
+private: 
+        void dfs(TreeNode* root, size_t level){
+                    
+                if(root== NULL)return;
+                sums.resize(max(sums.size(),level));
+                sums[level-1] += root->val;
+                dfs(root->left,level+1);
+                dfs(root->right,level+1);
+            }
 };
+
+
+//class Solution {
+//         // TC : O(N)
+//         // N -> NO. OF NODES IN THE TREE
+
+// public:
+//     int maxLevelSum(TreeNode* root) {
+        
+//             queue<TreeNode*> q;
+//             q.push(root);
+//             int max_sum = INT_MIN;
+//             int level = 0;
+//             int ans;
+//             while(!q.empty()){
+                    
+//                     level++;
+//                     int sum = 0;
+                    
+//                     for(int i = q.size()-1; i>=0; i--){
+                            
+//                             TreeNode* temp = q.front();
+//                             q.pop();
+                            
+//                             sum += temp->val;
+//                             if(temp->left) q.push(temp->left);
+//                             if(temp->right)q.push(temp->right);
+//                     }
+//                     if(sum>max_sum) {
+//                             max_sum = sum;
+//                             ans = level;
+//                     } 
+//             }
+//             return ans;
+//     }
+// };
