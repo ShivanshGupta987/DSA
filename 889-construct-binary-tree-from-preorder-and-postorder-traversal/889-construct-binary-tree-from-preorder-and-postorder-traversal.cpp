07a86bf699b59816579d01e271c3f0931c8b4d33
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-20 20:01:45 +0530

    Time: 18 ms (55.23%), Space: 24.4 MB (96.11%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -9,25 +9,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// USING ITERATIVE METHOD
+//         // TC : O(N)
+//         // SC : O(H)
+//         // N -> NO. OF NODES IN THE TREE
+//         // H -> HEIGHT OF THE TREE
 class Solution {
-        
-        // TC : O(N)
-        // SC : O(H)
-        // N -> NO. OF NODES IN THE TREE
-        // H -> HEIGHT OF THE TREE
-        
-        int pre_ind=0;
-        int pos_ind=0;
 public:
     TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-         
-            TreeNode* root = new TreeNode(pre[pre_ind++]);
-            
-            if(root->val != post[pos_ind]) 
-                    root->left = constructFromPrePost(pre,post);
-            if(root->val != post[pos_ind])
-                    root->right = constructFromPrePost(pre,post);
-            pos_ind++;
-            return root;
+        
+            vector<TreeNode*>s;
+            s.push_back(new TreeNode(pre[0]));
+            for(int i=1, j=0;i<pre.size();i++)
+            {
+                    TreeNode* root = new TreeNode(pre[i]);
+                    
+                    while(s.back()->val == post[j])
+                    {
+                           s.pop_back(); j++;
+                    }
+                    
+                    if(!s.back()->left) s.back()->left = root;
+                    else s.back()->right = root;
+                    s.push_back(root);
+            }
+            return s[0];
     }
 };
+
+
+
+// // USING DFS
+
+// class Solution {
+        
+//         // TC : O(N)
+//         // SC : O(H)
+//         // N -> NO. OF NODES IN THE TREE
+//         // H -> HEIGHT OF THE TREE
+        
+//         int pre_ind=0;
+//         int pos_ind=0;
+// public:
+//     TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+         
+//             TreeNode* root = new TreeNode(pre[pre_ind++]);
+            
+//             if(root->val != post[pos_ind]) 
+//                     root->left = constructFromPrePost(pre,post);
+//             if(root->val != post[pos_ind])
+//                     root->right = constructFromPrePost(pre,post);
+//             pos_ind++;
+//             return root;
+//     }
+// };
