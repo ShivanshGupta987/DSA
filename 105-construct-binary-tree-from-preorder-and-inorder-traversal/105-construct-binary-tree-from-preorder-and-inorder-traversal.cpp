5e92e9a71f2a808b179bc6e3bf3fb9db8ed77cf9
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-25 10:17:49 +0530

    Time: 14 ms (90.24%), Space: 26.4 MB (28.79%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,25 +10,49 @@
  * };
  */
 class Solution {
-        int pre_pos = 0;
-        int in_pos = 0;
+        
+unordered_map<int,int>mp;
+int pos = 0; // current index while traversing preorder array
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return dfs(preorder,inorder,INT_MIN);
-    }
         
+          for(int i=0;i<inorder.size();i++){
+                  mp.insert({inorder[i],i});
+          }
+          return dfs(preorder,0,preorder.size()-1);  
+                  
+    }
 private:
-        TreeNode* dfs(vector<int>& pre, vector<int>& in, int stop)
-        {
-                 if(pre_pos>=pre.size()) return NULL;
-                 if(stop==in[in_pos])
-                 {
-                         in_pos++;
-                         return NULL;
-                 }
-                 TreeNode* temp = new TreeNode(pre[pre_pos++]);
-                 temp->left = dfs(pre,in,temp->val);
-                 temp -> right = dfs(pre,in,stop);
-                 return temp;
-        }
+     TreeNode* dfs(vector<int>& preorder, int left, int right)
+     {
+             if(left>right) return NULL;
+             TreeNode* root = new TreeNode(preorder[pos++]);
+             root->left = dfs(preorder,left,mp[root->val]-1);
+             root->right = dfs(preorder,mp[root->val]+1,right);
+             return root;
+             }
 };
+
+// class Solution {
+//         int pre_ind = 0;
+//         int in_ind = 0;
+// public:
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         return dfs(preorder,inorder,INT_MIN);
+//     }
+// private: 
+//         TreeNode* dfs(vector<int>& preorder, vector<int>& inorder, int stop)
+//         {
+//                 if(pre_ind>=preorder.size()) return NULL;
+//                 if(inorder[in_ind]==stop) 
+//                 {
+//                         in_ind++; return NULL;
+//                 }
+                
+//                 TreeNode* root = new TreeNode(preorder[pre_ind++]);
+                
+//                 root->left = dfs(preorder,inorder,root->val);
+//                 root->right = dfs(preorder,inorder,stop);
+//                 return root;
+//         }
+// };
