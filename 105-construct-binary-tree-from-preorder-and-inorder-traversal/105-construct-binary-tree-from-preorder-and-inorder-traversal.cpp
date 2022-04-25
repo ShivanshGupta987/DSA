0ca949191fc2bf4821660be0cc6a0e91cbbcf07e
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-04-25 09:19:19 +0530

    Time: 24 ms (67.01%), Space: 25.9 MB (93.14%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,48 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// TC : O(N)
-// SC : O(H)
-// N -> NO. OF NODES IN THE TREE
-// H -> HEIGHT OF THE TREE
-
-// KEY IDEA :
-//                    let say we have a following subtree -
-                           
-//                            34
-//                           /  \
-//                          12   8
-//         as we traverse preorder array to make tree, if we go to the left of the root 
-//         then at some point of time we will encounter with 34 of inorder array it means 
-//         - 
-//         1) left subtree is done 
-//         2) Now just go one level up and fill the right of that root
-        
-//         hence as we go inside left subtree we set out stop target as 34 and for 
-//         right subtree we set ancestor of 34 as stop target
-
-
 class Solution {
-        int pre_ind = 0;
-        int in_ind = 0;
+        int pre_pos = 0;
+        int in_pos = 0;
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         return dfs(preorder,inorder,INT_MIN);
     }
-private: 
-        TreeNode* dfs(vector<int>& preorder, vector<int>& inorder, int stop)
+        
+private:
+        TreeNode* dfs(vector<int>& pre, vector<int>& in, int stop)
         {
-                if(pre_ind>=preorder.size()) return NULL;
-                if(inorder[in_ind]==stop) 
-                {
-                        in_ind++; return NULL;
-                }
-                
-                TreeNode* root = new TreeNode(preorder[pre_ind++]);
-                
-                root->left = dfs(preorder,inorder,root->val);
-                root->right = dfs(preorder,inorder,stop);
-                return root;
+                 if(pre_pos>=pre.size()) return NULL;
+                 if(stop==in[in_pos])
+                 {
+                         in_pos++;
+                         return NULL;
+                 }
+                 TreeNode* temp = new TreeNode(pre[pre_pos++]);
+                 temp->left = dfs(pre,in,temp->val);
+                 temp -> right = dfs(pre,in,stop);
+                 return temp;
         }
 };
