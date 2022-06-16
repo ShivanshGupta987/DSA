6b4de3cd4d1727af57bd9e582ecd3569620c4459
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-06-16 20:02:15 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (92.82%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -9,26 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// TC : O(N)
 class Solution {
-        TreeNode* ans = NULL;
-        TreeNode* pre = NULL;
-
+TreeNode* cur;
 public:
     TreeNode* increasingBST(TreeNode* root) {
-        
-            if(root->left) increasingBST(root->left);
-            TreeNode* cur = new TreeNode(root->val);
-            if(pre == NULL){
-                    pre = cur;
-                    ans = pre;
-            }
-            else{
-                    pre->right = cur;
-                    pre = pre->right;
-            }
-            if(root->right) increasingBST(root->right);
-            return ans;
-              
+        TreeNode* ans = new TreeNode(0);
+            cur = ans;
+            inorder(root);
+            return ans->right;
+    }
+
+    void inorder(TreeNode* node){
+            if(node==NULL)return;
+            inorder(node->left);
+            node->left = NULL;
+            cur->right = node;
+            cur = node;
+            inorder(node->right);
     }
 };
+
+
+// // TC : O(N)
+// class Solution {
+//         TreeNode* ans = NULL;
+//         TreeNode* pre = NULL; 
+// public:
+//     TreeNode* increasingBST(TreeNode* root) {
+        
+//             if(root->left) increasingBST(root->left);
+//             TreeNode* cur = new TreeNode(root->val);
+//             if(pre == NULL){
+//                     pre = cur;
+//                     ans = pre;
+//             }
+//             else{
+//                     pre->right = cur;
+//                     pre = pre->right;
+//             }
+//             if(root->right) increasingBST(root->right);
+//             return ans;
+              
+//     }
+// };
