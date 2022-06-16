52325c30d4bee8d81ef97845c8c2ff6e53cc28c1
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-06-16 23:14:19 +0530

    Time: 11 ms (90.12%), Space: 21.8 MB (31.89%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,16 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 // TC : O(N)
 // SC : O(N)
 class Solution {
+        // INORDER TRAVERSAL METHOD
 public:
-    bool isValidBST(TreeNode* root, long long int min_value = -1e10 , long long int max_value = 1e10) {
+    bool isValidBST(TreeNode* root) {
         
-            if(root==NULL)return true;
-            long long int val = root->val;
-            if(val<=min_value || val>=max_value) return false;
-            return isValidBST(root->left,min_value,val) && isValidBST(root->right, val, max_value);  
+            if(root == NULL ) return true;
+            stack<TreeNode*>s; 
+            TreeNode* pre = NULL;
+            
+            while(root || !s.empty()){                
+                    while(root){
+                            s.push(root);
+                            root = root->left;
+                    }
+                    
+                    root = s.top();
+                    s.pop();
+                    if(pre!= NULL && pre->val >= root->val) return false;
+                    pre = root;
+                    root = root->right;
+            }
+            return true;
+            
     }
 };
+
+
+// // TC : O(N)
+// // SC : O(N)
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root, long long int min_value = -1e10 , long long int max_value = 1e10) {
+        
+//             if(root==NULL)return true;
+//             long long int val = root->val;
+//             if(val<=min_value || val>=max_value) return false;
+//             return isValidBST(root->left,min_value,val) && isValidBST(root->right, val, max_value);  
+//     }
+// };
