b499afe8b77de7ddbc3844d28fd3ffc168668145
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-06-24 23:21:38 +0530

    Time: 7 ms (75.96%), Space: 13 MB (59.68%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -7,55 +7,117 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
-// TC : O(N)
-// SC : O(N)
 class Solution {
-vector<int>res;
-unordered_map<TreeNode*,int>mp;
+unordered_map<TreeNode*,TreeNode*>parent;
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
             
-            find(root,target);
+            dfs(root);
+            unordered_map<TreeNode*,bool>visited;
+            queue<TreeNode*>q;
+            q.push(target);
+            visited[target]=true;
+            int cur_level = 0;
+            while(!q.empty()){
+                    
+                    int size = q.size();
+                    if(cur_level++ == k)break;
+                    for(int i=0;i<size;i++){
+                                
+                            TreeNode* current = q.front();
+                            q.pop();
+                            
+                            if(current->left && !visited[current->left]){
+                                    q.push(current->left);
+                                    visited[current->left]=true;
+                            }
+                            
+                            if(current->right && !visited[current->right]){
+                                    q.push(current->right);
+                                    visited[current->right]=true;
+                            }
+                            
+                            if(parent[current] && !visited[parent[current]]){
+                                    q.push(parent[current]);
+                                    visited[parent[current]]=true;;
+                            }
+                            
+                
+                    }                
+            }
             
-            dfs(root,target,k,mp[root]);
+            vector<int>res;
+            while(!q.empty()){
+                    TreeNode* cur = q.front();
+                    q.pop();
+                    res.push_back(cur->val);
+            }
             return res;
         
-            }
-
-        int find(TreeNode* root,TreeNode* target){
-                if(root==NULL)return -1;
-                if(root==target){
-                        mp[root]=0;
-                        return 0;
-                }
-                int left = find(root->left,target);
-                if(left>=0){
-                        mp[root] = left+1;
-                        return left+1;
-                }
-                
-                int right = find(root->right,target);
-                if(right>=0){
-                        mp[root] = right+1;
-                        return right+1;
-                }
-                return -1;
-
-        }
+    }
         
-        void dfs(TreeNode* root, TreeNode* target, int k, int length){
-                
-                if(root == NULL)return;
-                
-                if(mp.count(root)){
-                        length = mp[root];
-                }
-                
-                if(length == k) res.push_back(root->val);
-                dfs(root->left,target,k,length+1);
-                dfs(root->right,target,k,length+1);
-
-                
-        }
+    void dfs(TreeNode* root){
+            
+            if(root->left){
+                    parent[root->left]=root;
+                    dfs(root->left);
+            }
+            
+            if(root->right){
+                    parent[root->right]=root;
+                    dfs(root->right);
+            }
+            
+    }
 };
+
+
+// TC : O(N)
+// SC : O(N)
+
+// class Solution {
+// vector<int>ans;
+// bool target_found = false;
+// bool target_over = false;
+// int target_val ;
+// int K;
+
+// public:
+//     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+//             target_val = target->val;
+//             K=k;
+//             dfs(root,k);
+//             return ans;
+            
+            
+//     }
+        
+//     void dfs(TreeNode* root, int k , int dist=0){
+       
+//             if(!root)return;
+//             if(k==0 && target_found){
+//                     ans.push_back(root->val);
+//                     return;
+//             } 
+//             if(dist==K && target_over){
+//                     ans.push_back(root->val);
+//             }
+//             if(root->val == target_val)
+//                     target_found = true;
+//             if(target_found){
+//                     dfs(root->left,k-1,dist);
+//                     dfs(root->right,k-1,dist);
+//             }
+//             if(root->val == target_val){
+//                     target_found = false;
+//                     target_found = true;
+//             }
+//             if(target_over){
+//                     dfs(root->left,-1,dist+1); 
+//                     dfs(root->right,-1,dist+1);
+//             }
+                    
+            
+//     }
+// };
