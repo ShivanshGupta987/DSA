649e1e6d4e44a07ed34ab82f3cd02fab03c5010e
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-09-01 23:34:25 +0530

    Time: 817 ms (12.55%), Space: 148.9 MB (23.34%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,15 +1,32 @@
 class Solution {
+    // TC : (n*log(n))
+    // SC : O(k)
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-        deque<int>dq;
+        // max heap to store to max element in the current window on the top
+        priority_queue<pair<int,int>>pq;
         vector<int>res;
-        for(int i=0;i<nums.size();i++){
-            if(!dq.empty() && i-dq.front()==k) dq.pop_front();
-            while(!dq.empty() && nums[i] >= nums[dq.back()]) dq.pop_back();
-            if(dq.empty() || nums[dq.back()] > nums[i]) dq.push_back(i);
-            if(i>=k-1) res.push_back(nums[dq.front()]);
+        
+        for(int i=0;i<k;i++){
+            pq.push({nums[i],i});
+        }
+        res.push_back(pq.top().first);
+        for(int i=k;i<nums.size();i++){
+            // push the current element of the window
+            pq.push({nums[i],i});
+            
+            // pop all the elements that don't belong to the current window
+            // current window [i-k+1,i]
+            
+            while(pq.top().second < i-k+1){
+                pq.pop();
+            }
+            
+            res.push_back(pq.top().first);
         }
         return res;
+        
+        
     }
 };
