200a2120fac0fd66f007cf356ae5a46e0692ca53
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2022-09-05 13:24:59 +0530

    Time: 14 ms (95.03%), Space: 15 MB (13.95%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,6 +1,6 @@
-// TC : O(n log(n-k))
+// ---------- USING BUCKET SORT -----------
 
-// ----------- USING MAX HEAP -------------
+// TC : O(n)
 
 class Solution {
 public:
@@ -9,20 +9,57 @@ public:
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        // max heap
-        priority_queue<pair<int,int>>pq;
-        vector<int>res;
+        int N = nums.size();
+        vector<int>bucket[N+1];
+        
         for(auto it=mp.begin();it!=mp.end();it++){
-            pq.push(pair(it->second,it->first));
-            if(pq.size() > mp.size()-k){
-                res.push_back(pq.top().second);
-                pq.pop();
-                if(res.size()==k)return res;
+            // int freq = it->second;
+            // int val = it->first;
+            bucket[it->second].push_back(it->first);
+        }
+        
+        vector<int>res;
+        for(int i=N;i>=0;i--){
+            int t = bucket[i].size();
+            // cout<<i<<" --- "<<t<<endl;
+            while(t--){
+                if(res.size()<k){
+                    res.push_back(bucket[i][t]);
+                }
+                if(res.size()==k) return res;
             }
         }
+        
         return res;
     }
 };
+
+
+// TC : O(n log(n-k))
+
+// ----------- USING MAX HEAP -------------
+
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int>mp;
+//         for(int i=0;i<nums.size();i++){
+//             mp[nums[i]]++;
+//         }
+//         // max heap
+//         priority_queue<pair<int,int>>pq;
+//         vector<int>res;
+//         for(auto it=mp.begin();it!=mp.end();it++){
+//             pq.push(pair(it->second,it->first));
+//             if(pq.size() > mp.size()-k){
+//                 res.push_back(pq.top().second);
+//                 pq.pop();
+//                 if(res.size()==k)return res;
+//             }
+//         }
+//         return res;
+//     }
+// };
 
 // ----------- USING MIN HEAP --------------
 
