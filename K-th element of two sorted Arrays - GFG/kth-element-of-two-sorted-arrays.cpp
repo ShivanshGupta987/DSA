eede16912b31d9fe7d2353acc227434c2fa8a4f7
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2023-05-19 20:41:25 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -5,41 +5,35 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
+    // TC : O(log(N) + log(M))
+    // SC : O(1)
+
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        int i=0,j=0;
-        int order=0;
-        int ans;
-        while(i<n && j<m){
-            order++;
-            if(arr1[i]<arr2[j]){
-                ans=arr1[i];
-                i++;
+        if(n>m) return kthElement(arr2,arr1,m,n,k);
+        int low = max(0,k-m), high = min(k,n);
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            int cut1 = mid;
+            int cut2 = k-cut1;
+            
+            int left1 = cut1<=0? INT_MIN : arr1[cut1-1];
+            int left2 = cut2<=0? INT_MIN : arr2[cut2-1];
+            int right1 = cut1>=n? INT_MAX : arr1[cut1];
+            int right2 = cut2>=m? INT_MAX : arr2[cut2];
+            
+                // cout<<cut1<<" "<<cut2<<"\n";
+            if(left1<=right2 && left2<=right1){
+                return max(left1,left2);
             }
-            else if(arr1[i]==arr2[j]){
-                ans=arr2[j];
-                i++;j++;
-                if(order==k)return ans;
-                order++;
+            else if(left1>right2){
+                high = mid-1;
             }
-            else{
-                ans=arr2[j];
-                j++;
-            }
-            if(order==k)return ans;
+            else low = mid+1;
         }
-        while(j<m){
-            ans=arr2[j];
-            order++;j++;
-            if(order==k)return ans;
-        }
-        while(i<n){
-            ans=arr1[i];
-            order++;i++;
-            if(order==k)return ans;
-        }
-        return ans;
+        return -1;
     }
 };
 
