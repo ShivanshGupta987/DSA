18cc761cc1818a332fe1d32dfba1fadaecadd036
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2023-07-07 14:56:50 +0530

    Added solution - LeetHub

---
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -0,0 +1,150 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+    
+Node *flatten(Node *root)
+{
+   // Your code here
+    
+    Node* head1 = root, *head2 = root->next;
+    while(head1 && head2){
+        Node* temp1 = head1->bottom, *temp2 = head2;
+        Node* prev = head1;
+        while(temp1 && temp2){
+            if(temp1->data <= temp2->data){
+                prev->bottom = temp1;
+                prev = temp1;
+                temp1 = temp1->bottom;
+            }
+            else{
+                prev->bottom = temp2;
+                prev = temp2;
+                temp2 = temp2->bottom;
+            }
+        }
+        while(temp1){
+            prev->bottom = temp1;
+            prev = temp1;
+            temp1 = temp1->bottom;
+        }
+        while(temp2){
+            prev->bottom = temp2;
+            prev = temp2;
+            temp2 = temp2->bottom;
+        }
+        head2 = head2->next;
+    }
+    return head1;
+   
+}
+
