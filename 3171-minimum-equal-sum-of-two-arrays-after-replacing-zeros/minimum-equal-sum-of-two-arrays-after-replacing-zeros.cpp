8e8e3c991ef3c4fe2da862291430d667279e6ad9
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-05-10 11:48:11 +0530

    Time: 125 ms (78.30%) | Memory: 135.4 MB (73.56%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -6,26 +6,19 @@ public:
         long long sum1 = 0, sum2 = 0, zc1 = 0, zc2 = 0;
         for(int i=0;i<nums1.size();i++){
             sum1 += nums1[i];
-            if(nums1[i]==0) zc1++;
+            if(nums1[i]==0) {
+                sum1++; zc1++;
+            }
         }
         for(int i=0;i<nums2.size();i++){
             sum2 += nums2[i];
-            if(nums2[i]==0) zc2++;
+            if(nums2[i]==0){
+                sum2++; zc2++;
+            } 
         }
-        if(zc1 && zc2) return max(sum1+zc1, sum2+zc2);
-        if(zc1==0 && zc2==0) return sum1==sum2? sum1 : -1;
-        if(zc1==0){
-            if(sum1<=sum2) return -1;
-            else if(zc2 > sum1-sum2) return -1;
-            else return sum1;
-        }
-        if(zc2==0){
-            if(sum2<=sum1) return -1;
-            else if(zc1 > sum2-sum1) return -1;
-            else return sum2;
-        }
+        if(!zc1 && sum2 > sum1 || !zc2 && sum1 > sum2) return -1;
         
-        return -1;
+        return max(sum1, sum2);
         
     }
 };
