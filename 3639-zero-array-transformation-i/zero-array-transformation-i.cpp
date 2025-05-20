fa3fd556368d3b9382e607d02c9df7630d65a5d9
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-05-21 00:25:26 +0530

    Time: 12 ms (48.32%) | Memory: 292.9 MB (51.17%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -8,12 +8,12 @@ public:
             temp[q[0]]--;
             temp[q[1]+1]++;
         }
-        nums[0] += temp[0];
-        if(nums[0]>0) return false;
+        
+        if(nums[0]+temp[0]>0) return false;
+
         for(int i=1;i<n;i++){
             temp[i] += temp[i-1];
-            nums[i] += temp[i];
-            if(nums[i]>0) return false;
+            if(nums[i]+temp[i]>0) return false;
         }
         return true;
     }
