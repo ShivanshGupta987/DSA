e894a79750747436fc925f624bbbcc6ffc380b85
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-05-25 08:54:22 +0530

    Time: 56 ms (9.97%) | Memory: 146.2 MB (16.11%) - LeetSync

---
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    // TC : O(N)
+    // SC : O(N)
+    vector<vector<long long>>dp;
+
+    long long getMax(vector<int>& nums, int ind, int k, int isEven){
+        int n = nums.size();
+        if(ind==n) return isEven==1?0:INT_MIN;
+        if(dp[ind][isEven]!=-1) return dp[ind][isEven];
+
+        
+        // doing xor on current index
+        long long sum1 = (nums[ind]^k) + getMax(nums, ind+1, k, isEven^1);
+
+        // not doing xor on current index
+        long long sum2 = nums[ind] + getMax(nums, ind+1, k, isEven);
+
+        return dp[ind][isEven] = max(sum1, sum2);
+        
+    }
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        int n = nums.size();
+        dp.resize(n, vector<long long>(2,-1));
+        return getMax(nums,0,k,1);
+       
+    }
+};
