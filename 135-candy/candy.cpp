57fdbf95fc4f3219650a1e6451c2e3b5c7e7254c
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-06-02 12:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (94.61%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,4 +1,41 @@
+// -------------- ONE PASS SOLUTION -------------
 class Solution {
+    // TC : O(N)
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int candy = n;
+        int i = 1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++; continue;
+            }
+
+            // processing increasing slope
+            int peak = 0;
+            while(ratings[i-1] < ratings[i]){
+                peak++;
+                candy += peak;
+                i++; 
+                if(i==n) return candy;
+            }
+
+            // processing decreasing slope
+            int valley = 0;
+            while(i<n && ratings[i-1] > ratings[i]){
+                valley++;
+                candy += valley;
+                i++;
+            }
+            candy -= min(peak, valley);
+        }
+        return candy;
+
+    }
+};
+
+//  ------------------ THREE PASS (ACTUALLY TWO PASS)  METHOD ----------
+class Solution1 {
     // TC : O(N)
 public:
     int candy(vector<int>& ratings) {
