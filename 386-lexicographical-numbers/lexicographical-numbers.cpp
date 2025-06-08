671a411e4e0aaffda79d9b7b0704937a37549b23
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-06-08 14:20:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (91.59%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,4 +1,33 @@
 class Solution {
+    // TC : O(N)
+    // SC : O(1)
+   
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int>res;
+        int cur_num = 1;
+        for(int i=0;i<n;i++){
+            res.push_back(cur_num);
+            // proceeding into depth
+            if(cur_num*10 <= n){
+                cur_num*=10;
+            }
+            // going for sibling node
+            else{
+                while(cur_num % 10 == 9 || cur_num >= n ){
+                    cur_num /= 10;
+                }
+                cur_num += 1;
+            }
+        }
+        return res;
+    }
+};
+
+//  ------------------ DFS(RECURSIVE) APPROACH ---------------
+class Solution1 {
+    // TC : O(N)
+    // SC : O(log N)
     void solve(vector<int>& res, int num, int maxx){
         for(int i=0;i<10;i++){
             int new_num = num*10 + i;
