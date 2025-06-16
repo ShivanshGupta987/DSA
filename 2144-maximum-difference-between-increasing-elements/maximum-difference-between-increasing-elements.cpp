667dc84077fe41cba368ac5f4396c44d87860d86
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-06-16 15:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (10.39%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -1,17 +1,15 @@
 class Solution {
     // TC : O(N)
-    // SC : O(N)
+    // SC : O(1)
 public:
     int maximumDifference(vector<int>& nums) {
-        vector<int>st;
+       
         int n = nums.size();
         int ans = -1;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && st.back() >= nums[i]){
-                st.pop_back();
-            }
-            st.push_back(nums[i]);
-            if(st.size()>1) ans = max(ans, st.back()-st[0]);
+        int premin = nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i] > premin) ans = max(ans, nums[i]-premin);
+            premin = min(premin, nums[i]);
         }
         return ans;
     }
