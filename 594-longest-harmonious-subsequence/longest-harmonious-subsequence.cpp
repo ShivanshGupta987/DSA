c6ea97306ac175b5a9cb1f95ea8cec958dff1e40
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-06-30 14:39:23 +0530

    Time: 10 ms (87.57%) | Memory: 36.4 MB (65.41%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -1,4 +1,28 @@
+// --------- SORTING BASED ----------
 class Solution {
+    // TC : O(n log n)
+    // SC : O(1)
+public:
+    int findLHS(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int cnt1 = 0, cnt2 = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(i==0 || nums[i]==nums[i-1])cnt1++;
+            else{
+                cnt2 = (nums[i]==nums[i-1]+1)?cnt1 : 0, cnt1 = 1;
+            }
+            if(cnt1>0 && cnt2>0) ans = max(ans, cnt1+cnt2);
+        }
+        return ans;
+    }
+};
+
+
+
+// --------------- HASH MAP BASED APPROACH ---------------
+class Solution1 {
     // TC : O(N)
     // SC : O(N)
 public:
