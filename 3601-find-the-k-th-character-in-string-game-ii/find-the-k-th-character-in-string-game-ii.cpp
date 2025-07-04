73d7ee895bd0e5a70d6675ba22e6f6acb8d026c5
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-07-05 02:36:12 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (41.94%) - LeetSync

--- 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
-    // TC : O(log2 k)
-    int get_ops(long long k){
+    // TC : O(log2 pos)
+    int get_floor(long long k){
         int cnt = 0;
         long long len = 1;
         while((len<<1) <= k){
@@ -10,14 +10,14 @@ class Solution {
         return cnt;
     }
 public:
-    char kthCharacter(long long k, vector<int>& operations) {
+    char kthCharacter(long long pos, vector<int>& operations) {
         
         int cnt = 0;
         // int cur_len = 1<<times;
-        long long pos = k;
+    
         int times;
         while(pos > 1){
-            times = get_ops(pos);
+            times = get_floor(pos);
             if((1ll << times) == pos) times--;
             pos -= (1ll << times);
             if(operations[times]) cnt++;
