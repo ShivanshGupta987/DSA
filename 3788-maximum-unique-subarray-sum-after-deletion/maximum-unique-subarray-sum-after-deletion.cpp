002bd1e8385f825ba2598a4609c4acdbaddc3008
Shivansh Gupta <90094267+ShivanshGupta02@users.noreply.github.com>
2025-07-26 03:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 29.8 MB (23.43%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    // TC : O(N)
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int>st;
+        int cur_sum = 0;
+        int res = INT_MIN;
+        for(int num : nums){
+            if(st.find(num)==st.end()){
+                res = max(res, cur_sum + num);
+                if(num>0) cur_sum += num; 
+            }
+            st.insert(num);
+        }
+        return res;
+    }
+};
