3343485c2c6273ff111f76524111e5e0da1e3c6e
Shivansh Gupta <90094267+ShivanshGupta987@users.noreply.github.com>
2025-10-05 14:30:04 +0530

    Time: 68 ms (58.90%) | Memory: 14.4 MB (50.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,4 +1,25 @@
+// ---------- DYNAMIC PROGRAMMING --------
 class Solution {
+    // TC : O(N^2)
+    // SC : O(N)
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,1);
+        int ans = 1;
+        for(int end=0;end<n;end++){
+            for(int start=0; start<end;start++){
+                if(nums[start] < nums[end]) dp[end] = max(dp[end], 1 + dp[start]);
+            }
+            ans = max(ans, dp[end]);
+        }
+        return ans;
+    }
+};
+
+// ---------- BINARY SEARCH --------
+
+class Solution1 {
     // TC : O(N log N)
     // SC : O(N)
 public:
