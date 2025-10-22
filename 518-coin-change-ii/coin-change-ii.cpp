3663b7e9b0ab872ca9e45e68087c44a2fa53261e
Shivansh Gupta <90094267+ShivanshGupta987@users.noreply.github.com>
2025-10-22 16:16:08 +0530

    Time: 30 ms (38.91%) | Memory: 16.6 MB (69.72%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -4,17 +4,14 @@ class Solution {
     // N : COINS.SIZE
 
     int dp[301][5001];
-    int rec(int idx, int amt, vector<int>& coins){
+    int solve(int idx, int amt, vector<int>& coins){
         int n = coins.size();
         if(amt==0) return 1;
-        if(idx>=n) return 0;
+        if(idx>=n || amt<0) return 0;
         if(dp[idx][amt]!=-1) return dp[idx][amt];
         int cnt = 0;
-        int j = 0;
-        while(j*coins[idx] <= amt){
-            cnt += rec(idx+1, amt-j*coins[idx], coins);
-            j++;
-        }
+        cnt += solve(idx+1, amt, coins);
+        cnt += solve(idx, amt-coins[idx], coins);
         return dp[idx][amt] = cnt;
 
     }
@@ -23,6 +20,6 @@ public:
         memset(dp, -1, sizeof(dp));
         int n = coins.size();
         sort(coins.begin(), coins.end());
-        return rec(0, amount, coins);
+        return solve(0, amount, coins);
     }
 };
