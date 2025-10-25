6a8a282827ec1240e9d7eab0b88a6e422b02dc37
Shivansh Gupta <90094267+ShivanshGupta987@users.noreply.github.com>
2025-10-25 23:49:37 +0530

    Time: 117 ms (7.07%) | Memory: 55 MB (5.14%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,21 +1,20 @@
 class Solution {
     // TC : O(M*N)
     vector<vector<int>>dp;
-    bool solve(int i, int j, string &s, string &t){
-        int n = s.size(), m = t.size();
+    bool solve(int i, int j, int n, int m, string &s, string &t){
         if(j==m && i==n) return true;
         else if(i>n) return false;
         if(dp[i][j]!=-1) return dp[i][j];
         bool ans = false;
         if(t[j]=='?'){
-            ans |= solve(i+1, j+1, s, t);
+            ans |= solve(i+1, j+1, n, m, s, t);
         }
         else if(t[j]=='*'){
-            ans =  solve(i+1, j, s, t) // taking '*' eq. to multiple char
-                || solve(i, j+1, s, t); // taking '*' eq. to empty string
+            ans =  solve(i+1, j, n, m, s, t) // taking '*' eq. to multiple char
+                || solve(i, j+1, n, m, s, t); // taking '*' eq. to empty string
         }
         else if(s[i]==t[j]){
-            ans |= solve(i+1, j+1, s, t);
+            ans |= solve(i+1, j+1, n, m, s, t);
         }
         else ans |= false;
         return dp[i][j] = ans;
@@ -24,6 +23,6 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
         dp = vector<vector<int>>(n+1, vector<int>(m+1, -1));
-        return solve(0, 0, s, p);
+        return solve(0, 0, n, m, s, p);
     }
 };
