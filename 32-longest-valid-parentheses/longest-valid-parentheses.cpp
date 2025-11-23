556d11dc5e3e98155e14d88b5f63093a255eab78
Shivansh Gupta <90094267+ShivanshGupta987@users.noreply.github.com>
2025-11-24 01:03:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (15.79%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,4 +1,27 @@
 class Solution {
+    // --------- USING DYNAMIC PROGRAMMING ----------
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        vector<int>dp(n); // dp[i] reprents len of longest substring ending at i
+        int ans = 0;
+        for(int i=1;i<n;i++){
+            if(s[i]=='(') continue;
+            if(s[i-1]=='('){
+                dp[i] = (i-2>=0?dp[i-2]:0) + 2;
+            }
+            else if(i-dp[i-1]-1>=0 && s[i-dp[i-1]-1]=='('){
+                dp[i] = (i-dp[i-1]-2>=0?dp[i-dp[i-1]-2] : 0) + dp[i-1] + 2;
+            }
+            ans = max(ans, dp[i]);
+        } 
+        return ans;
+    }
+};
+
+
+class Solution1 {
+    // ------------------ USING STACK -----------------------
     // TC : O(N)
 public:
     int longestValidParentheses(string s) {
