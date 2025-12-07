41e0033b33f029568718fb6a22a53618690034b5
Shivansh Gupta <90094267+ShivanshGupta987@users.noreply.github.com>
2025-12-07 20:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (91.32%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,4 +1,29 @@
 class Solution {
+    // ------------- BINARY SEARCH -------------
+    // TC : O(log N)
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        // binary search for range [0, n-k] for the finding the range 
+        // [left, left+k-1] while left can take the above defined range [0, n-k]
+        int left = 0, right = n-k;
+
+        while(left<right){
+            int mid = (left+right)/2;
+            // predicate(mid) == 0 then lies in left
+            if(x-arr[mid] > arr[mid+k]-x){
+                left = mid+1;
+            }
+            else right = mid;
+        }
+        return vector<int>(arr.begin()+left, arr.begin()+left+k);
+
+    }
+};
+
+
+
+class Solution1 {
     // ------------- TWO POINTER SOLUTION ------------
     // TC : O(N)
 public:
